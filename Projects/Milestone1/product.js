a1bd4929bc896b8baea7c4e33c9d420703318aa7
WittawwyBatman <26wilsojerp@washk12.org>
2025-01-30 12:05:32 -0700

    commit

--- Projects/Milestone1/product.js
+++ Projects/Milestone1/product.js
@@ -126,12 +126,11 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-        /* Open when someone clicks on the span element */
+    /* Open when someone clicks on the span element */
     function openNav() {
         document.getElementById("myNav").style.width = "100%";
     }
     
-    /* Close when someone clicks on the "x" symbol inside the overlay */
     function closeNav() {
         document.getElementById("myNav").style.width = "0%";
     }
