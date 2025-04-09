2e63f93b54dca2ceb7120967cebec60ac055e297
WittawwyBatman <26wilsojerp@washk12.org>
2025-04-09 08:51:51 -0600

    c

--- Altrui Website/main.js
+++ Altrui Website/main.js
@@ -40,19 +40,34 @@ if (form) {
   });
 }
 
-// Fit Guide Image Switcher
 const fitButtons = document.querySelectorAll('.fit-button');
 const fitImage = document.getElementById('fitImage');
+const leftBtn = document.querySelector('.angle-button.left');
+const rightBtn = document.querySelector('.angle-button.right');
 
-const fitImageMap = {
-  "slightly-oversized": "images/HeavyFitFront.png",
-  "heavyweight": "images/HeavyFitFront.png",
-  "boxy-cut": "images/HeavyFitFront.png"
+const imageAngles = ["front", "side", "back", "three-quarter"];
+let currentFit = "slightly-oversized";
+let currentAngleIndex = 0;
+
+const updateImage = () => {
+  const angle = imageAngles[currentAngleIndex];
+  fitImage.src = `/public/images/${currentFit}/${angle}.png`;
 };
 
 fitButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const selectedFit = button.getAttribute('data-fit');
-    fitImage.src = fitImageMap[selectedFit];
+    currentFit = button.getAttribute('data-fit');
+    currentAngleIndex = 0;
+    updateImage();
   });
+});
+
+leftBtn.addEventListener('click', () => {
+  currentAngleIndex = (currentAngleIndex - 1 + imageAngles.length) % imageAngles.length;
+  updateImage();
+});
+
+rightBtn.addEventListener('click', () => {
+  currentAngleIndex = (currentAngleIndex + 1) % imageAngles.length;
+  updateImage();
 });
